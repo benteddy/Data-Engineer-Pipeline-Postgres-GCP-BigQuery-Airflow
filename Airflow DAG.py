fde446561caba6b431e71f8182cec6f07ba64e17
Benhard <91111921+benteddy@users.noreply.github.com>
2021-09-28 21:08:35 +0700

    Update Airflow DAG.py

--- Airflow DAG.py
+++ Airflow DAG.py
@@ -1,80 +1,49 @@
-# Copyright 2018 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Data Engineer Pipeline Project using Postgres - GCP - BigQuery - Airflow
+The pipeline is using PostgreSQL as database and Google Cloud Storage as ETL datalake and BigQuery as access datalake. The scheduler is using Airflow.<br/><br/>
 
-"""An example DAG demonstrating simple Apache Airflow operators."""
+## Background
+My role is as a data engineer to create project for a small retail company. The retail company has a simple postgresql database. The management level wants to create a simple data warehouse and visualization using the google cloud platform and wants the data to be updated every day.
 
-from __future__ import print_function
-import datetime
-from airflow import models
-from airflow.operators import bash_operator
-from airflow.operators import python_operator
+Here is a simple data architecture that needs to be created:<br/><br/>
+<img src="Data Architecture.png" width="700" height="280"><br/><br/>
+The operational database has been prepared by the backend team. Here is a list of tasks to do:
+1. Retrieve data from postgresql and then save it into datalake in the form of csv file.
+2. The data in the data lake is loaded to enter the bigquery every day in the dataset.
+3. Perform script automation to retrieve data from postgresql to datalake (point 1) and from datalake to bigquery (point 2) at 01.00 every day using Apache Airflow (Google Composer) with the previous day's date. For historical data (before today), it is entered into datalake and data  warehouse in bulk (direct).
+4. Form a simple report:
+    - Report revenue per day:<br/>
+    <img src="Report Revenue per Day.png" width="500" height="38"><br/>
+    Gross revenue is the total price for all quantity per product minus the discount. Report is entered in the form of a table with the name dm_daily_gross_revenue. Here is the table schema used to this project from the postgres database.<br/>
+    <img src="Database Schema.png" width="400" height="310"><br/><br/>
+5. Create a visualization using Google Data Studio to display a revenue graph for the past 30 days.
 
-# [START composer_simple_define_dag]
-default_dag_args = {
-    # The start_date describes when a DAG is valid / can be run. Set this to a
-    # fixed point in time rather than dynamically, since it is evaluated every
-    # time a DAG is parsed. See:
-    # https://airflow.apache.org/faq.html#what-s-the-deal-with-start-date
-    'start_date': datetime.datetime(2021, 9, 17),
-}
+## Prerequisites
+These are all libraries you need to install
+- print_function
+- datetime
+- airflow
 
-# Define a DAG (directed acyclic graph) of tasks.
-# Any task you create within the context manager is automatically added to the
-# DAG object.
-with models.DAG(
-        'dag_benhard',
-        schedule_interval='0 1 * * *',
-        default_args=default_dag_args) as dag:
-        
-    def greeting():
-        import logging
-        logging.info('Hello World!')
-
-    initial_command_1 = bash_operator.BashOperator(
-        task_id='get_data_from_postgresql_orders_table',
-        bash_command='''export PGPASSWORD=jUDQgm5cLTQvoHd5;psql --set=sslmode=require -h shiftacademy-do-user-7592846-0.b.db.ondigitalocean.com -p 25061 -U doadmin -d shiftacademy -c "\copy (select * from orders where order_date = '{{ yesterday_ds }}') to '/home/airflow/gcs/data/benhard/orders/orders_{{ yesterday_ds }}.csv' CSV HEADER" ''')
-
-    initial_command_2 = bash_operator.BashOperator(
-        task_id='get_data_from_postgresql_order_details_table',
-        bash_command='''export PGPASSWORD=jUDQgm5cLTQvoHd5;psql --set=sslmode=require -h shiftacademy-do-user-7592846-0.b.db.ondigitalocean.com -p 25061 -U doadmin -d shiftacademy -c "\copy (select * from order_details od where order_id in (select distinct order_id from orders o2 where order_date = '{{ yesterday_ds }}')) to '/home/airflow/gcs/data/benhard/order_details/order_details_{{ yesterday_ds }}.csv' CSV HEADER" ''')
-
-    first_command_1 = bash_operator.BashOperator(
-        task_id='move_to_datalake_orders',
-        bash_command='gsutil cp gs://us-central1-shifacademy-b6818520-bucket/data/benhard/orders/orders_{{ yesterday_ds }}.csv gs://shiftacademy-final-project/benhard/orders/orders_{{ yesterday_ds }}.csv ')
-
-    first_command_2 = bash_operator.BashOperator(
-        task_id='move_to_datalake_order_details',
-        bash_command='gsutil cp gs://us-central1-shifacademy-b6818520-bucket/data/benhard/order_details/order_details_{{ yesterday_ds }}.csv gs://shiftacademy-final-project/benhard/order_details/order_details_{{ yesterday_ds }}.csv ')
-
-    second_command_1 = bash_operator.BashOperator(
-        task_id='upload_to_bigquery_orders',
-        bash_command='bq load --source_format=CSV --skip_leading_rows=1 benhard.orders gs://shiftacademy-final-project/benhard/orders/orders_{{ yesterday_ds }}.csv ')
-
-    second_command_2 = bash_operator.BashOperator(
-        task_id='upload_to_bigquery_order_details',
-        bash_command='bq load --source_format=CSV --skip_leading_rows=1 benhard.order_details gs://shiftacademy-final-project/benhard/order_details/order_details_{{ yesterday_ds }}.csv ')
-
-    # [END composer_simple_operators]
-
-    # [START composer_simple_relationships]
-    # Define the order in which the tasks complete by using the >> and <<
-    # operators. In this example, hello_python executes before goodbye_bash.
-    initial_command_1 >> first_command_1
-    first_command_1 >> second_command_1
-
-    initial_command_2 >> first_command_2
-    first_command_2 >> second_command_2
-
-    # [END composer_simple_relationships]
-# [END composer_simple]
+## Getting Started
+### Scheduler (Airflow – Google Cloud Composer)
+- **DAG Folder**<br/>
+The schedule script (dag) is stored in the dags folder. An example of a filename with the format is "Airflow DAG.py"<br/>
+<img src="DAG File Repository.png" width="400" height="155"><br/><br/>
+Here is the DAG script for Airflow process.<br/>`Airflow DAG.py`<br/><br/>
+- **Airflow Process**<br/>
+This is the "Tree View" of DAG.<br/>
+<img src="Airflow Process.png" width="850" height="355"><br/><br/>
+- **Temporary File**<br/>
+Temporary files from postgres database is stored in the "data>benhard" folder.<br/>
+<img src="Temporary File.png" width="420" height="150"><br/><br/>
+- **Datalake (Google Cloud Storage)**<br/>
+The files from temporary is stored in the "benhard" directory.<br/>
+Bucket name :               -final-project<br/>
+Directory : "benhard"<br/>
+<img src="Datalake (Google Data Storage).png" width="650" height="155"><br/><br/>
+- **Data Warehouse (BigQuery)**<br/>
+The file from Google Cloud Storage is loaded to datalake in BigQuery.<br/>
+Dataset name : "benhard"<br/>
+<img src="Data Warehouse (BigQuery).png" width="245" height="50"><br/><br/>
+### Visualisasi (Google Data Studio)
+Data visualization is created from dataset in BigQuery
+<img src="Data Visualization (Google Data Studio).png" width="650" height="450"><br/><br/>
